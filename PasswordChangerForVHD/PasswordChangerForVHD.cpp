36f6f49a5443efde1ecb1fc5b94bb482a32c8183
Antoine Cauchois <antoinepico@msn.com>
2019-04-14 22:56:20 +0200

    Add first Main
    This commit is the very first one as a beginning of the project.

---
+++ PasswordChangerForVHD/PasswordChangerForVHD.cpp
@@ -0,0 +1,68 @@
+/* --- INCLUDES ------------------------------------------------------------- */
+#include "PasswordChangerForVHD.h"
+
+/* --- PRIVATE VARIABLES ---------------------------------------------------- */
+static PCFVHD_OPTIONS gs_sOptions = { 0 };
+
+/* --- PUBLIC VARIABLES ----------------------------------------------------- */
+
+/* --- PRIVATE FUNCTIONS ---------------------------------------------------- */
+__declspec(noreturn) static void PCFVHDUsage(
+    _In_ const PTCHAR ptProgName,
+    _In_opt_ const PTCHAR ptMsg
+    )
+{
+    if (ptMsg != NULL)
+    {
+        _tprintf_s(_T("Error: "));
+        _tprintf_s(_T("%s"), ptMsg);
+    }
+
+    _tprintf_s(_T("Usage: "));
+    _tprintf_s(_T("%s -d <drive letter> -u <username> -p <password>\r\n"), ptProgName);
+
+    ExitProcess(EXIT_FAILURE);
+}
+
+static void PCFVHDParseOptions(
+    _In_ const PPCFVHD_OPTIONS pOpt,
+    _In_ const INT argc,
+    _In_ const PTCHAR argv[]
+) {
+    INT curropt = 0;
+
+    while ((curropt = getopt(argc, argv, _T("d:u:p:"))) != -1)
+    {
+        switch (curropt)
+        {
+            case _T('d'):
+                pOpt->ptDriveLetter = optarg;
+                break;
+            case _T('u'):
+                pOpt->ptLogin = optarg;
+                break;
+            case _T('p'):
+                pOpt->ptLogin = optarg;
+                break;
+
+            default:
+                ExitProcess(EXIT_FAILURE);
+        }
+    }
+}
+
+INT _tmain(
+    _In_ const INT argc,
+    _In_ const PTCHAR argv[]
+    )
+ {
+
+    // Option
+    if (argc == 1) {
+        PCFVHDUsage(argv[0], NULL);
+    }
+
+    PCFVHDParseOptions(&gs_sOptions, argc, argv);
+
+    return EXIT_SUCCESS;
+}
---
+++ PasswordChangerForVHD/PasswordChangerForVHD.h
@@ -0,0 +1,19 @@
+#ifndef __PCFVHD_H__
+#define __PCFVHD_H__
+
+/* --- INCLUDES ------------------------------------------------------------- */
+#include <tchar.h>
+#include <windows.h>
+#include "../Utils/getopt.h"
+
+/* --- DEFINES -------------------------------------------------------------- */
+
+/* --- TYPES ---------------------------------------------------------------- */
+
+typedef struct _PCFVHD_OPTIONS {
+    PTCHAR ptDriveLetter;
+    PTCHAR ptLogin;
+    PTCHAR ptPassword;
+} PCFVHD_OPTIONS, *PPCFVHD_OPTIONS;
+
+#endif
